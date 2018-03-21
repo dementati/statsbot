e965cbfb2577e38dfda865886ad20da9486e4277
Lucas Lindström <ztargaser@gmail.com>
2018-03-21 22:19:43 +0100

    Log parser with support for different encodings.

---
+++ statsbot/Log.py
@@ -0,0 +1,74 @@
+import re
+
+encodings = ["utf8", "latin_1"]
+
+class Log:
+
+    def __init__(self, **kwargs):
+        self.clear()
+
+        if "filename" in kwargs:
+            if not self.parse_file(kwargs["filename"]):
+                success = False
+                for encoding in encodings:
+                    if self.parse_file(kwargs["filename"], encoding):
+                        success = True
+                        break
+
+                if not success:
+                    raise Exception("Couldn't parse file: " + kwargs["filename"])
+
+        elif "line" in kwargs:
+            self.entries = [self.parse(kwargs["line"])]
+
+        self.entries = [entry for entry in self.entries if entry]
+
+    def parse_file(self, filename, encoding=None):
+        try:
+            self.clear()
+
+            if encoding:
+                file = open(filename, encoding=encoding)
+            else:
+                file = open(filename)
+
+            self.entries = [self.parse(line) for line in file.readlines()]
+            return True
+        except UnicodeDecodeError:
+            return False
+
+    def clear(self):
+        self.unparsed = []
+        self.entries = []
+
+    def parse(self, line):
+        m = re.match("^(\S*)\s<([^>]*)> (.*)$", line)
+
+        if not m:
+            self.unparsed.append(line)
+            return None
+
+        if len(m.groups()) != 3:
+            self.unparsed.append(line)
+            return None
+
+        return {"timestamp" : m.groups()[0], "nick" : m.groups()[1], "message" : m.groups()[2]}
+
+    def __str__(self):
+        return str(self.entries)
+
+
+if __name__ == "__main__":
+    log = Log(line="[00:04] <rosvi> I have a linux box I'm gonna install arch a windows box. Not bootable media. Would it be possible to installa distro on those circumstances? Just wondering about it")
+    log = Log(filename="#ubuntu.txt")
+    log = Log(filename="log.txt")
+    log = Log(filename="palle_pig.txt")
+    [print(entry) for entry in log.entries]
+
+    '''
+    log = Log(line="11:39 < Scarecrow> I'm so desensitized to cakes. You get cake all the time nowadays.")
+    print(log)
+
+    log = Log(filename="log.txt")
+    [print(entry) for entry in log.entries]
+    '''
---
+++ statsbot/Stats.py
@@ -0,0 +1,12 @@
+from Log import Log
+
+class Stats:
+
+    def __init__(self, log):
+        pass
+
+
+if __name__ == "__main__":
+    log = Log(filename="palle_pig.txt")
+    print(log.unparsed)
+    print(log)
