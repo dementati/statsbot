e69cb1687e5d62c2a5e290cec640383562ecfcef
Lucas Lindström <lucas.lindstrom@cinnober.com>
2018-03-23 05:56:01 +0100

    Compute bad word percentage instead of bad word count

--- statsbot/Stats.py
+++ statsbot/Stats.py
@@ -13,12 +13,21 @@ def print_list(list):
 class Stats:
 
     def __init__(self, log):
+        self.word_count_per_nick = defaultdict(int)
         self.message_distribution_over_nicks = defaultdict(float)
-        self.bad_word_count_per_nick = defaultdict(int)
+        self.bad_word_percentage_per_nick = defaultdict(float)
         self.bad_words_per_nick = defaultdict(lambda: defaultdict(int))
 
+        self.calculate_word_count_per_nick(log)
         self.calculate_message_distribution_over_nicks(log)
-        self.calculate_on_bad_words(log)
+        self.calculate_on_bad_words(log, self.word_count_per_nick)
+
+    def calculate_word_count_per_nick(self, log):
+        for entry in log.entries:
+            words = entry["message"].split()
+            words = [word.strip(".") for word in words]
+            words = [word.lower() for word in words]
+            self.word_count_per_nick[entry["nick"]] += len(words)
 
     def calculate_message_distribution_over_nicks(self, log):
         messages_per_user = defaultdict(int)
@@ -28,9 +37,11 @@ class Stats:
         for nick in messages_per_user.keys():
             self.message_distribution_over_nicks[nick] = messages_per_user[nick] / len(log.entries)
 
-    def calculate_on_bad_words(self, log):
+    def calculate_on_bad_words(self, log, word_count_per_nick):
         with open(bad_words_file) as file:
             all_bad_words = [word.strip() for word in file.readlines()]
+
+        bad_word_count_per_nick = defaultdict(int)
 
         for entry in log.entries:
             # Words
@@ -42,17 +53,21 @@ class Stats:
             for word in bad_words:
                 self.bad_words_per_nick[entry["nick"]][word] += 1
 
-            self.bad_word_count_per_nick[entry["nick"]] += len(bad_words)
+            bad_word_count_per_nick[entry["nick"]] += len(bad_words)
+
+        for nick in bad_word_count_per_nick.keys():
+            if word_count_per_nick[nick] > 1000 and bad_word_count_per_nick[nick] / word_count_per_nick[nick] > 0:
+                self.bad_word_percentage_per_nick[nick] = bad_word_count_per_nick[nick] / word_count_per_nick[nick]
 
 
 if __name__ == "__main__":
     stats = Stats(Log(filename="../resources/palle_pig.txt"))
 
-    bad_word_count = descending_map(stats.bad_word_count_per_nick)
+    bad_word_count = descending_map(stats.bad_word_percentage_per_nick)
 
     for i in range(10):
-        nick = bad_word_count[i][0]
+        nick, percentage = bad_word_count[i]
 
-        print("Foulmouth #%d: %s" % (i+1, nick))
+        print("Foulmouth #%d: %s, percentage: %f%%" % (i+1, nick, percentage*100))
 
         print_list(descending_map(stats.bad_words_per_nick[nick])[0:10])
